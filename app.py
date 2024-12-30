202060a9b432b25d56ecffdd78b99c6bc7b76081
zunairarehman <zunairareh1@gmail.com>
2024-12-30 18:58:09 +0500

    init

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-@app.route("/website")
+@app.route("/")
 def website():
-    return render_template("website.html")
+    return render_template("index.html")
 
 @app.route("/membership")
 def membership():
